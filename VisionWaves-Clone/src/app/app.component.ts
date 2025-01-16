b25b2871fa079f6c58aaa5a34ab5bf146f0ceb7e
thowhid45 <thowhid268989@>
2025-01-16 21:14:53 +0530

    failed

--- VisionWaves-Clone/src/app/app.component.ts
+++ VisionWaves-Clone/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone:true
 })
 export class AppComponent {
   title = 'VisionWaves-Clone';
+  
 }
